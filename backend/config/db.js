cc374cef7baa23866030c262516a780b05bbaded
yadavnarendra1128 <yadavnarendra1137@gmail.com>
2025-01-28 15:22:14 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,16 @@
+require("dotenv").config(); // Load environment variables from the .env file
+
 const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 60000, // 60 seconds timeout
-  socketTimeoutMS: 60000, // 60 seconds socket timeout
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log("MongoDB connection error:", err));
-
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 60000, // 60 seconds timeout
+      socketTimeoutMS: 60000, // 60 seconds socket timeout
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB connection error:", err));
+  console.log("database connected successfully!");
+};
+module.exports = connectDB;
