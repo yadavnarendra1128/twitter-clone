37c5af78d1dd534fa3c481ddada12f82baf944c8
yadavnarendra1128 <yadavnarendra1137@gmail.com>
2025-01-28 14:12:47 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
-
-// Old code (with deprecated options)
-mongoose.connect(process.env.MONGO_URI, {
-});
-
 // Updated code (no deprecated options)
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{serverSelectionTimeoutMS: 60000,)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
