5874a48389aa8ce7aedd126a6ddd4a7ec9f16b42
yadavnarendra1128 <yadavnarendra1137@gmail.com>
2025-01-28 15:00:37 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 60000, // 60 seconds timeout
+  socketTimeoutMS: 60000, // 60 seconds socket timeout
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000,
-  socketTimeoutMS: 30000,
 })
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log("MongoDB connection error:", err));
+
